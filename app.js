e4162a9a1ab064a9569b55e24b7f7f2f2f33627c
armanaydemir <arman.aydemir@live.com>
2015-07-31 14:37:03 -0400

    started adding redis models

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var redis = require('redis');
+var redisClient = redis.createClient();
+const User = require('redis-user')(redisClient);
+var session_store = require('connect-mongo')(require('express'));
 
 app.use(express.cookieParser());
-app.use(express.session({secret: 'iWonderIfAnyoneWouldGuessThis'}));
+app.use(express.session({
+	store: new MongoStore({
+    	url: 'mongodb://root:myPassword@mongo.onmodulus.net:27017/3xam9l3'
+  	}),
+  	secret: 'iWonderIfAnyoneWouldGuessThis'
+}));
 
 app.get('/', function(req, res){
-	
 	res.sendFile(__dirname + '/index.html')
 })
