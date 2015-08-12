e85fd7d7d361470e82788e393135260de07e7c84
Rielle <rmq1895@gmail.com>
2015-08-12 19:25:54 -0400

    added competition navbar, changes made to dashboard

---
+++ views/_/components/php/comp-navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button><!-- button -->
+            <a class="navbar-brand" href="#">TCSC</a>
+        </div> <!--/.nav-collapse -->
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#">Questions</a></li>
+                <li><a href="#">Leaderboard</a></li>
+                <li><a href="#">Rules</a></li>
+                <!-- perhaps we should move the rules to the settings -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Your Profile</a></li>
+                        <li><a href="#">Team Profile</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Settings</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Log Out</a></li>
+                    </ul> <!-- dropdown-menu -->
+                </li> <!-- dropdown -->
+            </ul> <!-- navbar links -->
+        </div> <!-- code for collapsable navbar -->
+    </div> <!-- container -->
+</nav> <!-- navbar -->
--- views/privacypolicy.php
+++ views/privacypolicy.php
@@ -11,7 +11,7 @@
     <link href="_/css/indexstyles.css" rel="stylesheet">
       
   </head>
-  <body id="index">
+  <body id="privacypolicy">
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
             <div class="cover-container">
