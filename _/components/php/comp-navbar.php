974f4199bf838f00fb1b913fdc1d4941379c8213
Rielle <rmq1895@gmail.com>
2015-08-13 22:54:53 -0400

    competition dashboard initial setup done

--- _/components/php/comp-navbar.php
+++ _/components/php/comp-navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -7,25 +7,26 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button><!-- button -->
-            <a href="#" class="pull-left logo">
-                <img style="max-width:40px;max-height: 40px;margin-top: 5px"src="images/emblem.png" id="logodiv">
-            </a>
-            <a href="#" class="navbar-brand hidden-xs hidden-sm">Triangle Computer Science Competition</a>
             
         </div> <!--/.nav-collapse -->
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#">Questions</a></li>
-                <li><a href="#">Leaderboard</a></li>
-                <li><a href="#">Rules</a></li>
+                <a href="#" class="pull-left logo" style="padding-right:3em;">
+                    <img style="max-width:40px;max-height: 40px;margin-top: 5px"src="images/emblem.png" id="logodiv">
+                </a>
+                <li class="active"><a href="#">Dashboard</a></li>
+                <li><a href="#">Stats</a></li>
+                <li><a href="#">Team</a></li>
                 <!-- perhaps we should move the rules to the settings -->
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span><span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        <img src="images/useridenticon.png" class="img-rounded" alt="id" width="20px" height="20px">
+                        <span class="caret"></span>
+                    </a>
                     <ul class="dropdown-menu">
                         <li class="dropdown-header">Signed in as "username"</li>
                         <li role="separator" class="divider"></li>
                         <li><a href="#">Your Profile</a></li>
-                        <li><a href="#">Team Profile</a></li>
                         <li><a href="#">Help</a></li>
                         <li role="separator" class="divider"></li>
                         <li><a href="#">Settings</a></li>
